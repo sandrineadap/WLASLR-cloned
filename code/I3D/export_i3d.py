58d28e2ea474adc4c04bee33319f61bc63af9c94
sandrine vr-lab <sandrine@andrews.edu>
2024-04-04 22:45:23 -0400

    stuck trying to create a ptl file. It's having rpoblems reading InceptionI3D from pytorch_i3d (so i copied the file and modified but still no luck

--- code/I3D/export_i3d.py
+++ code/I3D/export_i3d.py
@@ -7,5 +7,17 @@
 import torch
 import torchvision 
 from torch.utils.mobile_optimizer import optimize_for_mobile
+from pytorch_i3d_torchOps import InceptionI3d
+# from pytorch_i3d import InceptionI3d
 
-#model = 
+
+model = InceptionI3d(400, in_channels=3)
+# model = torchvision.models.mobilenet_v3_small(pretrained=True)
+model.eval()
+
+scripted_model = torch.jit.script(model)
+optimized_model = optimize_for_mobile(scripted_model)
+optimized_model._save_for_lite_interpreter("i3d_small.ptl")
+# optimized_model._save_for_lite_interpreter("mobilenet_v3_small.ptl")
+
+print("model successfully exported")
